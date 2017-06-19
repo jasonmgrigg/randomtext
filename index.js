40f9d5a6d082621b95e25ff527fac0cc5c09f2ba
Jason Grigg <boatdreamin@gmail.com>
2017-06-19 13:24:44 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// console.log('hello from Node.js')
 const express = require('express');
 const app = express();
 
 var loremIpsum = require('lorem-ipsum')
     output = loremIpsum({
-    count: 3                      // Number of words, sentences, or paragraphs to generate.
-  , units: 'paragraphs'            // Generate words, sentences, or paragraphs.
-  , sentenceLowerBound: 3         // Minimum words per sentence.
-  , sentenceUpperBound: 5        // Maximum words per sentence.
-  , paragraphLowerBound: 4        // Minimum sentences per paragraph.
-  , paragraphUpperBound: 6        // Maximum sentences per paragraph.
-  , format: 'plain'               // Plain text or html
+    count: 3
+  , units: 'paragraphs'
+  , sentenceLowerBound: 3
+  , sentenceUpperBound: 5
+  , paragraphLowerBound: 4
+  , paragraphUpperBound: 6
+  , format: 'plain'
   , suffix: "###########"
 });
 
@@ -18,10 +17,6 @@ app.get('/lorem', function(req, res) {
   res.send(output);
 });
 
-// app.get('/goodbye', function(req, res) {
-//   res.send('See You Later!');
-// });
-
 app.listen(3000, function() {
-  console.log("Successfully started express application!")
+  console.log("Watch out, you're running!")
 });
